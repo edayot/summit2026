03bfc72d5de08712a7115f068bca8a0b41b59bfd
edayot <pro.e.dayot@gmail.com>
2026-04-18 16:27:57 +0200

    new screen

---
+++ link.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+
+RP=/home/erwan/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/26.1/minecraft/resourcepacks
+DP=/home/erwan/sftp/paralya/sftp/summit/datapacks
+
+
+uv run beet l --data-pack $DP --resource-pack $RP
--- model_resolver_summit/plugin.py
+++ model_resolver_summit/plugin.py
@@ -1,8 +1,27 @@
 from beet import Context
 from simple_item_plugin.types import NAMESPACE, Lang
-from simple_item_plugin.item import Item
+from simple_item_plugin.item import Item, BlockProperties
 from simple_item_plugin.crafting import ShapedRecipe, VanillaItem
+import weld_deps
 
 
 def beet_default(ctx: Context):
-    beet_item = Item(id="beet", item_name=(f"{NAMESPACE}:beet", {Lang.en_us: ""})).export(ctx)
+    beet_item = Item(id="beet", item_name=(f"{NAMESPACE}:beet", {Lang.en_us: "Beet Item"})).export(ctx)
+
+
+    screen = Item(
+        id="screen", 
+        base_item="soul_campfire",
+        item_name=(f"{NAMESPACE}:beet", {Lang.en_us: "Beet Item"}),
+        block_properties=BlockProperties(
+            base_block="minecraft:soul_campfire",
+            block_states={
+                "facing": "south",
+                "lit": "false",
+                "signal_fire": "true",
+                "waterlogged": "false",
+            },
+            all_same_faces=False,
+            entity_type="marker",
+        ),
+    ).export(ctx)
