2957028b02648cb34b16a8dcbcac34e777e53b51
edayot <pro.e.dayot@gmail.com>
2026-04-19 10:40:30 +0200

    display animation

--- model_resolver_summit/plugin.py
+++ model_resolver_summit/plugin.py
@@ -1,7 +1,66 @@
-from beet import Context
+from beet import Context, Font, Texture
+from beet.core.utils import JsonDict
 from simple_item_plugin.types import NAMESPACE, Lang
 from simple_item_plugin.item import Item, BlockProperties
 from simple_item_plugin.crafting import ShapedRecipe, VanillaItem
+
+from beet.contrib.messages import Message
+
+from model_resolver.render import Render
+from model_resolver.tasks.model import AnimatedResultTask
+from PIL import Image
+
+
+
+def create_animation_text(ctx: Context, id: str):
+    path = f"minecraft:block/{id}"
+    render = Render(ctx, default_render_size=256)
+    task = render.add_model_task(path)
+    render.run()
+
+    font: JsonDict = {
+        "providers": [
+            {
+                "type": "reference",
+                "id": "minecraft:include/space"
+            }
+        ]
+    }
+    font_path = f"{NAMESPACE}:font"
+
+    text: list[JsonDict | str] = [""]
+
+    char_index = 0xe000
+    char_offset = 0x0004
+    for i, t in enumerate(task.tasks):
+        assert isinstance(t.saved_img, Image.Image)
+        char_index += char_offset
+        char_item = f"\\u{char_index:04x}".encode().decode("unicode_escape")
+
+        render_path = f"{NAMESPACE}:item/font/{id}/{i}"
+        ctx.assets.textures[render_path] = Texture(t.saved_img)
+
+        font["providers"].append({
+            "type": "bitmap",
+            "file": f"{render_path}.png",
+            "ascent": 8,
+            "height": 16,
+            "chars": [char_item],
+        })
+        text.append({
+            "text": char_item + " ",
+            "font": font_path,
+            "color": "white",
+        })
+        n = 4
+        if i%n == n-1:
+            text.append("\n\n")
+
+        
+    ctx.assets.fonts[font_path] = Font(font)
+
+    ctx.data[Message][f"{NAMESPACE}:{id}"] = Message(text)
+
 
 
 def beet_default(ctx: Context):
@@ -13,7 +72,10 @@ def beet_default(ctx: Context):
         base_item="furnace",
         item_name=(f"{NAMESPACE}:screen", {Lang.en_us: "Screen"}),
         block_properties=BlockProperties(
-            base_block="minecraft:oxidized_copper_bars",
+            base_block="minecraft:barrier",
             entity_type="item_display",
         ),
     ).export(ctx)
+
+    create_animation_text(ctx, "sculk_sensor")
+
--- src/data/model_resolver_summit/function/impl/custom_block_ext/on_place/screen/place_entity.mcfunction
+++ src/data/model_resolver_summit/function/impl/custom_block_ext/on_place/screen/place_entity.mcfunction
@@ -1,10 +1,10 @@
+
+from beet.contrib.messages import Message
+
 data merge entity @s {transformation: {scale: [2.001f, 2.001f, 2.001f]}}
 
 execute summon text_display run function ./place_text_display: 
-    res = ""
-    for i in range(12):
-        res = res + (chr(65+i)) * 37 + "\n"
-    res = res[:-1]
 
-    data merge entity @s {Tags:["model_resolver_summit.screen.text_display"], text: res, line_width: 500, background: 0}
+    text = ctx.data[Message]["model_resolver_summit:sculk_sensor"].data
+    data merge entity @s {Tags:["model_resolver_summit.screen.text_display"], text: text, line_width: 500, background: 0}
     tp @s ~ ~-0.8 ~ ~ ~
