ee3aafd0686d462e5d5120c4c97063d5ec709f0c
edayot <pro.e.dayot@gmail.com>
2026-06-21 17:46:09 +0200

    sky1

--- link.sh
+++ link.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 
-RP=/home/erwan/.var/app/org.prismlauncher.PrismLauncher/data/PrismLauncher/instances/26.1/minecraft/resourcepacks
+RP=/home/erwan/.local/share/PrismLauncher/instances/26.1/minecraft/resourcepacks
 DP=/home/erwan/sftp/paralya/sftp/summit/datapacks
 
 
